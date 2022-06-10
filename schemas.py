ca9d8cbfbb2a0e17bd558c1a878028c818184d63
DBSantiago <deblasissantiago@gmail.com>
2022-06-10 16:33:50 -0300

    ADD: User username length validation

--- schemas.py
+++ schemas.py
@@ -1,6 +1,14 @@
 from pydantic import BaseModel
+from pydantic import validator
 
 
 class UserBaseModel(BaseModel):
     username: str
     password: str
+
+    @validator("username")
+    def validate_username(cls, username):
+        if not 3 <= len(username) <= 50:
+            raise ValueError("Username must be between 3 and 50 characters long.")
+
+        return username
