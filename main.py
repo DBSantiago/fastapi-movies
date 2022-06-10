7902e4eb04a316ed50c8b65b67645337de8a3ae9
DBSantiago <deblasissantiago@gmail.com>
2022-06-10 16:01:37 -0300

    ADD: Database connection and User, Movie, Review models

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from database import database as connection
+from database import User, Movie, Review
 
 app = FastAPI(title="Movies Reviews", description="A project where we can review movies", version="1")
 
@@ -6,11 +8,17 @@ app = FastAPI(title="Movies Reviews", description="A project where we can review
 @app.on_event("startup")
 def startup():
     print("Server is starting up...")
+    if connection.is_closed():
+        connection.connect()
+
+    connection.create_tables([User, Movie, Review])
 
 
 @app.on_event("shutdown")
 def shutdown():
     print("Server is shutting down...")
+    if not connection.is_closed():
+        connection.close()
 
 
 @app.get("/")
@@ -21,3 +29,8 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.post("/users")
+async def create_user():
+    pass
