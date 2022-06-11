64afd471588d5d4ba5da1d3b73dfd164220c65f7
DBSantiago <deblasissantiago@gmail.com>
2022-06-11 03:29:09 -0300

    ADD: POST, PUT and DELETE requests on client.py

--- client.py
+++ client.py
@@ -8,6 +8,16 @@ QUERY_SET = {
     "page": 1,
     "limit": 2
 }
+REVIEW = {
+    "user_id": 7,
+    "movie_id": 1,
+    "review_text": "It left me speechless",
+    "score": 5
+}
+response = requests.post(URL_REVIEWS, json=REVIEW)
+
+if response.status_code == 200:
+    print(f"Review created successfully")
 
 response = requests.get(URL_REVIEWS, headers=HEADERS, params=QUERY_SET)
 
@@ -23,3 +33,20 @@ if response.status_code == 200:
 
         for review in reviews:
             print(f"Movie: {review['movie']['title']} - Score: {review['score']}")
+
+REVIEW_ID = 10
+UPDATED_REVIEW = {
+    "review_text": "It was pretty good",
+    "score": 4
+}
+response = requests.put(URL_REVIEWS + f"/{REVIEW_ID}", json=UPDATED_REVIEW)
+
+if response.status_code == 200:
+    print("PUT Request successful")
+    print(response.json())
+
+response = requests.delete(URL_REVIEWS + f"/{REVIEW_ID}")
+
+if response.status_code == 200:
+    print("DELETE Request successful")
+    print(response.json())
