0968c205ab8fa10433c1eff1c49a407f26835177
DBSantiago <deblasissantiago@gmail.com>
2022-06-11 03:10:08 -0300

    ADD: Client.py to check our endpoints

---
+++ client.py
@@ -0,0 +1,21 @@
+import requests
+
+URL_REVIEWS = "http://localhost:8000/api/v1/reviews"
+HEADERS = {
+    "accept": "application/json"
+}
+
+response = requests.get(URL_REVIEWS, headers=HEADERS)
+
+if response.status_code == 200:
+    print("Request successful")
+    print(response.content)
+    print(response.headers)
+
+    if response.headers.get("content-type") == "application/json":
+        print(response.json())
+
+        reviews = response.json()
+
+        for review in reviews:
+            print(f"Movie: {review['movie']['title']} - Score: {review['score']}")
