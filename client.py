4fa5a8d3924ca2fe38216476e99193d040d7d64f
DBSantiago <deblasissantiago@gmail.com>
2022-06-11 03:15:27 -0300

    ADD: Query set

--- client.py
+++ client.py
@@ -4,8 +4,12 @@ URL_REVIEWS = "http://localhost:8000/api/v1/reviews"
 HEADERS = {
     "accept": "application/json"
 }
+QUERY_SET = {
+    "page": 1,
+    "limit": 2
+}
 
-response = requests.get(URL_REVIEWS, headers=HEADERS)
+response = requests.get(URL_REVIEWS, headers=HEADERS, params=QUERY_SET)
 
 if response.status_code == 200:
     print("Request successful")
