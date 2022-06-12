67e4c7450b038c649a999a0aede8242b9557d2c1
DBSantiago <deblasissantiago@gmail.com>
2022-06-12 19:08:09 -0300

    ADD: POST request with login and cookies on client.py

--- client.py
+++ client.py
@@ -14,39 +14,85 @@ REVIEW = {
     "review_text": "It left me speechless",
     "score": 5
 }
-response = requests.post(URL_REVIEWS, json=REVIEW)
 
-if response.status_code == 200:
-    print(f"Review created successfully")
+# ======================================
+#             POST REQUEST
+# ======================================
 
-response = requests.get(URL_REVIEWS, headers=HEADERS, params=QUERY_SET)
+# response = requests.post(URL_REVIEWS, json=REVIEW)
+#
+# if response.status_code == 200:
+#     print(f"Review created successfully")
 
-if response.status_code == 200:
-    print("Request successful")
-    print(response.content)
-    print(response.headers)
+# ======================================
+#             GET REQUEST
+# ======================================
 
-    if response.headers.get("content-type") == "application/json":
-        print(response.json())
+# response = requests.get(URL_REVIEWS, headers=HEADERS, params=QUERY_SET)
+#
+# if response.status_code == 200:
+#     print("Request successful")
+#     print(response.content)
+#     print(response.headers)
+#
+#     if response.headers.get("content-type") == "application/json":
+#         print(response.json())
+#
+#         reviews = response.json()
+#
+#         for review in reviews:
+#             print(f"Movie: {review['movie']['title']} - Score: {review['score']}")
 
-        reviews = response.json()
+# ======================================
+#             PUT REQUEST
+# ======================================
 
-        for review in reviews:
-            print(f"Movie: {review['movie']['title']} - Score: {review['score']}")
+# REVIEW_ID = 10
+# UPDATED_REVIEW = {
+#     "review_text": "It was pretty good",
+#     "score": 4
+# }
+# response = requests.put(URL_REVIEWS + f"/{REVIEW_ID}", json=UPDATED_REVIEW)
+#
+# if response.status_code == 200:
+#     print("PUT Request successful")
+#     print(response.json())
 
-REVIEW_ID = 10
-UPDATED_REVIEW = {
-    "review_text": "It was pretty good",
-    "score": 4
+# ======================================
+#             DELETE REQUEST
+# ======================================
+
+# response = requests.delete(URL_REVIEWS + f"/{REVIEW_ID}")
+#
+# if response.status_code == 200:
+#     print("DELETE Request successful")
+#     print(response.json())
+
+# ======================================
+#        POST REQUEST - COOKIES
+# ======================================
+URL_LOGIN = "http://localhost:8000/api/v1/users/login"
+URL_USER_REVIEWS = "http://localhost:8000/api/v1/users/reviews"
+USER = {
+    "username": "username",
+    "password": "password"
 }
-response = requests.put(URL_REVIEWS + f"/{REVIEW_ID}", json=UPDATED_REVIEW)
+response = requests.post(URL_LOGIN, json=USER)
 
 if response.status_code == 200:
-    print("PUT Request successful")
+    print("User authenticated successfully")
+
     print(response.json())
 
-response = requests.delete(URL_REVIEWS + f"/{REVIEW_ID}")
+    user_id = response.cookies.get_dict().get("user_id")
+    print(user_id)
 
-if response.status_code == 200:
-    print("DELETE Request successful")
-    print(response.json())
+    cookies = {
+        "user_id": user_id
+    }
+
+    response = requests.get(URL_USER_REVIEWS, cookies=cookies)
+
+    if response.status_code == 200:
+        for review in response.json():
+            print(f"Movie: {review['movie']['title']} - Score: {review['score']}")
